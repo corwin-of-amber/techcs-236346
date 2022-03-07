4ff5f20bb207f57cb9e4c7a8a7135eda7e717879
Shachar Itzhaky <corwin.amber@gmail.com>
2022-03-07 17:03:24 +0200

    [bugfix] Reversed image.
    (at least relative to the order of the bits in my implementation.)

--- src/sim.ts
+++ src/sim.ts
@@ -22,7 +22,10 @@ class Simulation extends EventEmitter {
     _processLine(ln: string) {
         var mo = ln.match(/^([0-9a-f]+)\|([01]+)$/i);
         if (mo) {
-            this.emit('video:out', {y: parseInt(mo[1], 16), data: new BitSet(mo[2])});
+            this.emit('video:out', {
+                y: parseInt(mo[1], 16), 
+                data: new BitSet([...mo[2]].reverse().join(''))  /* BitSet is MSB-first */
+            });
         }
     }
 }
