237de8d5b54115efb5d14243c38411e1c517dd4c
Shachar Itzhaky <corwin.amber@gmail.com>
2022-03-18 15:04:09 +0200

    [bugfix] Only include jQuery if running in IPython.
    Skip silently if IPython is not installed (also graphviz).

--- hw/base/boilerplate.py
+++ hw/base/boilerplate.py
@@ -5,9 +5,17 @@ collections.Mapping = collections.abc.Mapping
 # For jupyterlab:
 
 # - include jQuery
-from IPython.display import display, HTML, Javascript
-display(HTML('<script src="https://unpkg.com/jquery@3.3.1/dist/jquery.js"></script>'))
+try:
+    import IPython
+    if IPython.get_ipython():
+        from IPython.display import display, HTML
+        display(HTML('<script src="https://unpkg.com/jquery@3.3.1/dist/jquery.js"></script>'))
+except ImportError:
+    pass
 
 # - monkey-patch graphviz
-from graphviz import Source
-Source._repr_svg_ = Source._repr_image_svg_xml
+try:
+    from graphviz import Source
+    Source._repr_svg_ = Source._repr_image_svg_xml
+except ImportError:
+    pass
