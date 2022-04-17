5219ff7ea4f76aaa43f56cd31fe36aca5c9439d4
Shachar Itzhaky <corwin.amber@gmail.com>
2022-04-17 17:53:38 +0300

    [port] Added download & extract script.
    To hopefully cope with Windows (that does not have wget
    and tar).

---
+++ etc/download_prebuilt.mjs
@@ -0,0 +1,29 @@
+/*
+ * Downloads `a2sui.tar.gz` and unpacks it.
+ * Written in JS to increase portability.
+ */
+
+import fs from 'fs';
+import fetch from 'node-fetch';
+import tar from 'tar';
+
+
+const URI = 'https://adder2snake.vercel.app/a2sui.tar.gz',
+      TAR_FN = 'a2sui.tar.gz';
+
+async function main() {
+    console.log('📦 Downloading ' + URI);
+    let resp = await fetch(URI);
+    const dest = fs.createWriteStream(TAR_FN);
+    resp.body.pipe(dest);
+    await new Promise((resolve, reject) => {
+        resp.body.on("end", () => resolve("it worked"));
+        dest.on("error", reject);
+    });
+    var cnt = 0;
+    await tar.x({file: TAR_FN, onentry: () => cnt++});
+    console.log(`🗂  Done (${cnt} files extracted).`);
+}
+
+
+main();
