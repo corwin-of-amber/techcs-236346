d52bb8155f1d8bae9162fa9d4b9f7175e945fe6b
Shachar Itzhaky <corwin.amber@gmail.com>
2022-04-26 15:18:56 +0300

    [feature] Video adapter peripheral.

---
+++ hw/cpu/periph.py
@@ -0,0 +1,17 @@
+from pyrtl import Register, Output, concat
+
+
+# Video adapter
+VIDEO_BASE = 0xa000
+
+def video_adapter(mem, base_addr=VIDEO_BASE):
+    scan = Register(bitwidth=8, name="vid_scan")
+    vy = Output(bitwidth=8, name="vid_y")
+    out = Output(bitwidth=256, name="vid_out")
+    scan.next <<= scan + 1  # wraps around
+    out <<= concat(*(mem[(base_addr + concat(scan, as_wires(i, bitwidth=4)))[:16]]
+                   for i in reversed(range(16))))
+    vy <<= scan
+    
+
+
