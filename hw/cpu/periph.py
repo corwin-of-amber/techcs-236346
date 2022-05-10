afbd9978c16668eff84c5fd7ec808fe089df913c
Shachar Itzhaky <corwin.amber@gmail.com>
2022-05-10 15:37:10 +0300

    [feature] Added GPIO adapter.

--- hw/cpu/periph.py
+++ hw/cpu/periph.py
@@ -15,3 +15,15 @@ def video_adapter(mem, base_addr=VIDEO_BASE):
     
 
 
+# GPIO adapter
+GPIO_BASE = 0xc000
+
+def gpio_adapter(mem, base_addr=GPIO_BASE):
+    gpio_in = Input(bitwidth=32, name='gpio_in')
+    gpio_out = Output(bitwidth=32, name='gpio_out')
+
+    mem[base_addr + 0x0] <<= gpio_in[:16]
+    mem[base_addr + 0x1] <<= gpio_in[16:]
+    gpio_out <<= concat(mem[base_addr + 0x3], mem[base_addr + 0x2])
+    return gpio_in, gpio_out
+
