43c185d68bda0b6103c1d098fc7bcc98fb3663c2
Shachar Itzhaky <corwin.amber@gmail.com>
2025-05-08 17:01:19 +0300

    [bugfix] Previous fix broke labels.

--- src/emulation/assembler.ts
+++ src/emulation/assembler.ts
@@ -10,13 +10,13 @@ class Assembler {
         var mo: RegExpMatchArray | RegExpExecArray;
         for (mo of text.matchAll(/[^\n]+/g)) {
             let line = mo[0].trim();
-            if (mo = line.match(/(\S+)(?:\s+(.*))?/)) {
+            if (mo = line.match(/(.*):$/)) {
+                yield mo[1];
+            }
+            else if (mo = line.match(/(\S+)(?:\s+(.*))?/)) {
                 let [, opcode, arg] = mo, num = parseInt(arg),
                     nums = opcode === 'YANK' ? arg.split(/,/).map(x => parseInt(x)) : null;
                 yield [opcode, nums ?? (isNaN(num) ? arg : num)];
-            }
-            else if (mo = line.match(/(.*):$/)) {
-                yield mo[1];
             }
             else {
                 console.error('(in asm) ', line);
