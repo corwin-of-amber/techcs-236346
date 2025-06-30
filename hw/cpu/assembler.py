d07dc081f9016133fbdc58b9245f4fde048544ca
Shachar Itzhaky <corwin.amber@gmail.com>
2025-06-30 16:57:44 +0300

    [oops] Updated assembler to be real.

--- hw/cpu/assembler.py
+++ hw/cpu/assembler.py
@@ -4,15 +4,32 @@ import instruction_set as isa
 
 
 class Assembly(namedtuple('Assembly', ['words', 'start_addr', 'label_addrs'])):
+
     def to_bin(self):
         assert _array_sanity()  # ensure that 'I' marks 32-bit words
         return array.array('I', self.words).tobytes()
+
     def save_bin(self, filename='a.bin'):
         with open(filename, 'wb') as f:
             f.write(self.to_bin())
 
+    @classmethod
+    def from_bin(cls, bin, start_addr=0x0):
+        assert _array_sanity()  # ensure that 'I' marks 32-bit words
+        a = array.array('I'); a.frombytes(bin)
+        return cls(words=list(a), start_addr=start_addr, label_addrs={})
+
+    @classmethod
+    def load_bin(cls, filename, start_addr=0x0):
+        with open(filename, 'rb') as f:
+            return cls.from_bin(f.read())
+
 class Label:
-    def __init__(self, l): self.name = l
+    def __init__(self, l):
+        self.name = l[:-1] if l.endswith(':') else l
+
+# these opcodes take a 16-bit arg
+WIDE_ARG = [isa.PUSH, isa.JMP, isa.JZ, isa.JNZ]
 
 
 def asm_bin(l, start_addr=0x0):
@@ -35,25 +52,36 @@ def asm(l, start_addr=0x0):
         for instr in l:
             if isinstance(instr, Label):
                 labels[instr.name] = addr
+            elif isinstance(instr, tuple):
+                l = warg(*instr)
+                addr += len(l)
+                yield from l
             else:
-                yield instr
                 addr += 1
+                yield instr
 
-    def warg(op, arg):
+    def lookup(name):
+        try: return labels[name]
+        except: raise ValueError(f'label not found: `{name}`')
+    
+    def warg(op, arg=0):
         if isinstance(op, str): op = isa.CODES[op]
         if isinstance(arg, Label): arg = labels[arg.name]
         elif isinstance(arg, str):
-            arg = isa.CODES[arg] if op == isa.ALU else labels[arg]
+            arg = isa.CODES[arg] if op == isa.ALU else Label(arg)
         elif isinstance(arg, tuple):
             arg = (arg[1] << 4) | arg[0]
-        return (arg << 4) | op
-                
+        if op in WIDE_ARG:
+            return [op, arg]
+        else:
+            return [(arg << 4) | op]
+
     def third_pass(l):
-        for instr in l:
-            if isinstance(instr, tuple):
-                yield warg(*instr)
+        for w in l:
+            if isinstance(w, Label):
+                yield lookup(w.name)
             else:
-                yield instr
+                yield w
                 
     l = list(second_pass(first_pass(l)))
     return Assembly(list(third_pass(l)), start_addr, labels)
